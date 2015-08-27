f4e3fc05f103d623c2f24572740c21beab35ca2c
Jimin Sung <jimin@Jimins-MacBook-Pro.local>
2015-08-27 10:04:29 -0400

    Most specs passing

--- public/app.js
+++ public/app.js
@@ -26,3 +26,33 @@ app.value('whateverName', [
         ]
     }
 ]);
+
+app.factory('FlashCardsFactory', function ($http) {
+    return {
+      getFlashCards: function(category) {
+        var queryParams = {};
+
+        if (category) {
+            queryParams.category = category;
+        }
+
+        return $http.get ('/cards', {
+          params: queryParams
+        })
+          .then(function(res){
+            return res.data
+          })
+      }
+    };
+});
+
+app.controller('StatsController', function ($scope, ScoreFactory) {
+    $scope.scores = ScoreFactory;
+});
+
+app.factory('ScoreFactory', function () {
+    return {
+        correct: 0,
+        incorrect: 0
+    };
+});
--- public/main.controller.js
+++ public/main.controller.js
@@ -1,10 +1,41 @@
-app.controller('MainController', function ($scope, whateverName) {
-	$scope.flashCards = whateverName;
+app.controller('MainController', function($scope, FlashCardsFactory, ScoreFactory) {
+  FlashCardsFactory.getFlashCards()
+    .then(function(cards) {
+      $scope.flashCards = cards;
+			$scope.numCorrect = 0;
+			$scope.numWrong = 0;
+    });
 
-	$scope.answerQuestion = function (answer, flashCard) {
-		if (!flashCard.answered) {
-			flashCard.answered = true;
-			flashCard.answeredCorrectly = answer.correct;
-		}
+  $scope.answerQuestion = function(answer, flashCard) {
+    if (!flashCard.answered) {
+      flashCard.answered = true;
+      flashCard.answeredCorrectly = answer.correct;
+			answer.correct ? ScoreFactory.correct++ : ScoreFactory.incorrect++;
+    }
+  }
+
+  $scope.categoryFinder = function(category) {
+    FlashCardsFactory.getFlashCards(category)
+      .then(function(cards) {
+        $scope.flashCards = cards;
+				$scope.selectedCat = category;
+      });
+  }
+
+	$scope.resetAll = function(){
+		FlashCardsFactory.getFlashCards()
+	    .then(function(cards) {
+	      $scope.flashCards = cards;
+				$scope.selectedCat = null;
+	    });
 	}
+
+  $scope.categories = [
+    'MongoDB',
+    'Express',
+    'Angular',
+    'Node'
+  ];
+
+
 });
